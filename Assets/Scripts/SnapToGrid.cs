1169fa8d495e9f4d792bc600e3606edf18c57146
Chi Shen <shenchi710@gmail.com>
2016-09-05 21:54:08 -0400

    imported art assets

---
+++ Assets/Scripts/SnapToGrid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SnapToGrid : MonoBehaviour
+{
+#if UNITY_EDITOR
+    public Vector3 offset = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            transform.position = new Vector3(Mathf.Floor(transform.position.x), Mathf.Floor(transform.position.y), Mathf.Floor(transform.position.z)) + offset;
+        }
+    }
+#endif
+}
